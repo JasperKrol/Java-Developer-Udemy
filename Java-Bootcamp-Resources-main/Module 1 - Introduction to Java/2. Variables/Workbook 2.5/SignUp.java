6b623d85080bb744ee98d73f0d8f248d60c1ac84
Jasper Paul Krol <jasper.paul.krol@gmail.com>
2022-05-16 17:49:35 +0200

    oefening 2.5 done

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
@@ -11,11 +11,24 @@ public class SignUp {
         //Task 1 - Using Scanner, ask the user questions
 
         //Ask for their first name.
+        System.out.println("your name pls");
+        String name = scan.nextLine();
         //Ask for their last name.
+        System.out.println("last name pls");
+        String lastname = scan.nextLine();
         //Ask: how old are you?
+        System.out.println("age");
+        int age = scan.nextInt();
         //Ask them to make a username.
+        System.out.println("choose username");
+        String username = scan.nextLine();
         //Ask what city they live in.
+        System.out.println("city u live");
+        String city = scan.nextLine();
+
         //Ask what country that's from.
+        System.out.println("country u live in");
+        String country = scan.nextLine();
 
 
         //Task 2 - Print their information. 
@@ -23,7 +36,18 @@ public class SignUp {
         System.out.println("Thank you for joining JavaGram!");
 
         //Print their information like so:
+        System.out.println("Thank you for joining JavaGram!");
 
+        System.out.println("\nHere is the information you entered:");
+        System.out.println("\tFirst Name: " +name);
+        System.out.println("\tLast Name: " + lastname);
+        System.out.println("\tAge: " + age);
+        System.out.println("\tUsername: " + username);
+        System.out.println("\tCity: " + city);
+        System.out.println("\tCountry: " + country);
+
+        //close scanner. It's good practice :D !
+        scan.close();
         // Your information:
         //        First Name: Rayan
         //        Last Name: Slim
@@ -34,6 +58,6 @@ public class SignUp {
         //
 
 
-        //close scanner. It's good practice :D ! 
+
     }
 }
