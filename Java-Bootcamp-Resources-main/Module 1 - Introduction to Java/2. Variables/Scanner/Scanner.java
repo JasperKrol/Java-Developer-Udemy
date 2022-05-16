bf3c4ff77ca38e28f33847dca8f884fae732642d
Jasper Paul Krol <jasper.paul.krol@gmail.com>
2022-05-16 17:43:26 +0200

    scanner part 2 done \n

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Scanner/Scanner.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Scanner/Scanner.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Scanner {
+public class Scanner {
+
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.println("\nwelcome blablalba");
+        System.out.println("\nwhat is your name?");
+        String name = scanner.nextLine();
+        System.out.println("\nyour name = " + name);
+        System.out.println("\nhow much do you spend on bidoe FF");
+        double costs = scanner.nextDouble();
+        System.out.println("\nkosten" + costs);
+
+        // belangrijk als je klaar bent met de input van de scanner
+        scanner.close();
+
+        // mogelijke oplossing voor . of , in je scanner
+//        import java.util.Locale;
+        //
+        //        Scanner scan = new Scanner(System.in).useLocale(Locale.ENGLISH);
+    }
+
+
 }
