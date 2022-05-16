fa99c89ee5627f5f9c8f24bcf377b134ce54c1a9
Jasper Paul Krol <jasper.paul.krol@gmail.com>
2022-05-16 17:39:51 +0200

    section 2 scanner pt1 done

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/MathOperations and TypeCasting/TypeConversion.java
@@ -0,0 +1,10 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+
+        double salary = 5099.335;
+        int roundedSalary = (int) salary;
+
+        System.out.println("he earns " + roundedSalary);
+
+    }
+}
